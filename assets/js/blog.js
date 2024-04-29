b860ac354e765658192b61a4648fa0a923a86221
koliandrik <koliandrik@gmail.com>
2024-04-29 15:52:38 -0700

    added portfolio link;

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,6 +5,7 @@ const posts = document.createElement('div');
 const footer = document.createElement('footer');
 const modeBtn = document.createElement('button');
 const backBtn = document.createElement('button');
+const portfolio = document.createElement('button');
 
 modeBtn.className = 'btn';
 modeBtn.id = 'mode';
@@ -13,6 +14,12 @@ backBtn.className = 'btn';
 backBtn.id = 'back';
 
 posts.className = 'container';
+
+portfolio.className = 'btn';
+portfolio.id = 'portfolio';
+portfolio.textContent = 'Portfolio';
+
+footer.appendChild(portfolio);
 
 body.appendChild(header);
 body.appendChild(main);
@@ -36,7 +43,7 @@ if (storedItems) {
     blogPosts = JSON.parse(storedItems);
     for (let i = 0; i < blogPosts.length; i++) {
         const post = document.createElement('div');
-        post.className = 'post';
+        post.className = 'card';
         posts.appendChild(post);   
         const h2 = document.createElement('h2');
         const h3 = document.createElement('h3');
@@ -50,3 +57,7 @@ if (storedItems) {
         post.appendChild(p);
     }
 }
+
+portfolio.addEventListener('click', function() {
+    window.location = 'https://koliandrik.github.io/Week-2/';
+});
